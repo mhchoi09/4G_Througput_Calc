c5e1af7f32a43f7eedb8dc4bb2db797a10303430
rhuin <rhui21@live.com.ph>
2021-05-18 12:26:39 +0800

    alpha

---
+++ bandwidth.py
@@ -0,0 +1,47 @@
+table_bw = {
+    1.4: {
+        'resource_block': 6,
+        'subcarriers': 72,
+        'uplink': 1.08,
+        'downlink': 1.095,
+    },
+    3: {
+        'resource_block': 15,
+        'subcarriers': 180,
+        'uplink': 2.7,
+        'downlink': 2.715,
+    },
+    5: {
+        'resource_block': 25,
+        'subcarriers': 300,
+        'uplink': 4.5,
+        'downlink': 4.515,
+    },
+    10: {
+        'resource_block': 50,
+        'subcarriers': 600,
+        'uplink': 9.0,
+        'downlink': 9.015,
+    },
+    15: {
+        'resource_block': 75,
+        'subcarriers': 900,
+        'uplink': 13.5,
+        'downlink': 13.515,
+    },
+    20: {
+        'resource_block': 100,
+        'subcarriers': 1200,
+        'uplink': 18,
+        'downlink': 18.015,
+    },
+}
+
+table_resource_block = {
+    1: {
+        'subcarrier': 12,
+        'bandwidth': 180
+    },
+}
+
+
